4c3167c4358bb4b4a5afc1f72174309099af4849
Agney Anand <58386546+full4metal@users.noreply.github.com>
2025-08-19 16:08:59 +0530

    30969

--- home/models.py
+++ home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Restaurant(models.Model):
+    name = models.charField(max_length=200)
+
+    def __str__(self):
+        return self.name
